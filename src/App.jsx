398c91668d82f091bc684976bb32b2f035b0332b
GribuzLord <gribuzcode@gmail.com>
2025-05-05 23:29:26 -0600

    Se crearon los principios de la logica para la funcion addToCart en donde se comprueba si un elemento ya habia sido agregado previamente al carrito

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ function App() {
   const [cart,setCart]=useState([])
 
   function addToCart(item){
+    const itemExists=cart.findIndex(guitar=>guitar.id===item.id)
+    console.log(itemExists)
     setCart(prevCart=>[...prevCart,item])
   }
 
