a23db8186c1e17e6bb9f560bd80a4fe054fba812
GribuzLord <gribuzcode@gmail.com>
2025-05-07 19:46:27 -0600

    Se creó un custom hook en el cual se migró toda la logica que originalmente estaba en el app.jsx, se importo y se hicieron cambios minimos; todo esto con el fin de brindar una mejor comprension y un futuro mantenimiento mas ordenado

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,11 @@
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
-import { useState,useEffect } from "react"
-import { db } from "./data/db"
+import { useCart } from "./hooks/useCart"
 
 
 function App() {
 
-  const initialCart = () => {
-    const localStorageCart=localStorage.getItem('cart') //Busca en local storage
-
-    return localStorageCart ? JSON.parse(localStorageCart) : [] //Si tiene algo convierte ese valor y si no el valor inicial esta vacio
-  }
-
-  //State
-  const [data]=useState(db) //El que modifica el estado es la variable de la derecha (setAuth)
-  const [cart,setCart]=useState(initialCart)
-
-  const MAXITEMS=5
-
-  useEffect(()=>{//Similar a la funcion watch, va a ejecutar lo que este dentro cada que detecte cambios en cierto componente
-    localStorage.setItem('cart',JSON.stringify(cart));
-  },[cart])
-
-
-
-  function addToCart(item){
-
-    const itemExists=cart.findIndex(guitar=>guitar.id===item.id)
-
-    if(itemExists>=0){//existe en el carrito
-      if(cart[itemExists].quantity>=MAXITEMS) return
-      const updatedCart=[...cart]
-      updatedCart[itemExists].quantity++
-      setCart(updatedCart)
-
-    }else{
-      item.quantity=1
-      setCart([...cart,item])
-    }
-  }
-
-  function removeFromCart(id){
-    setCart(prevCart=>prevCart.filter(guitar=>guitar.id!==id))
-  }
-
-  function increaseQuantity(id){
-    const updatedCart=cart.map(item=>{
-      if(item.id===id && item.quantity<MAXITEMS){
-        return{
-          ...item,
-          quantity:item.quantity+1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  function decreaseQuantity(id){
-    const updatedCart=cart.map(item=>{
-      if(item.id===id && item.quantity>1){
-        return{
-          ...item,
-          quantity:item.quantity-1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  function clearCart(e){
-    setCart([])
-  }
-
-
+  const{data, cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart}=useCart()
 
 
   return (
@@ -96,14 +27,12 @@ function App() {
         <div className="row mt-5">
       {/* Un prop es la manera en la que se comunican entre si los diferentes componentes de react */}
           {data.map((guitar)=>( 
-             <Guitar 
+            <Guitar 
               key={guitar.id}
               guitar={guitar}
-              setCart={setCart}
               addToCart={addToCart}
-             />
+            />
           ))}
-         
         </div>
     </main>
     <footer className="bg-dark mt-5 py-5">
---
+++ src/hooks/useCart.js
@@ -0,0 +1,85 @@
+import { useState,useEffect } from "react"
+import { db } from "../data/db"
+
+export const useCart=()=>{
+
+    const initialCart = () => {
+        const localStorageCart=localStorage.getItem('cart') //Busca en local storage
+        return localStorageCart ? JSON.parse(localStorageCart) : [] //Si tiene algo convierte ese valor y si no el valor inicial esta vacio
+    }
+    
+      //State
+    const [data]=useState(db) //El que modifica el estado es la variable de la derecha (setAuth)
+    const [cart,setCart]=useState(initialCart)
+    
+    const MAXITEMS=5
+    
+    useEffect(()=>{//Similar a la funcion watch, va a ejecutar lo que este dentro cada que detecte cambios en cierto componente
+        localStorage.setItem('cart',JSON.stringify(cart));
+    },[cart])
+    
+    
+    
+    function addToCart(item){
+    
+        const itemExists=cart.findIndex(guitar=>guitar.id===item.id)
+        
+        if(itemExists>=0){//existe en el carrito
+            if(cart[itemExists].quantity>=MAXITEMS) return
+            const updatedCart=[...cart]
+            updatedCart[itemExists].quantity++
+            setCart(updatedCart)
+        
+            }else{
+            item.quantity=1
+            setCart([...cart,item])
+            }
+    }
+    
+    function removeFromCart(id){
+        setCart(prevCart=>prevCart.filter(guitar=>guitar.id!==id))
+    }
+    
+    function increaseQuantity(id){
+        const updatedCart=cart.map(item=>{
+        if(item.id===id && item.quantity<MAXITEMS){
+            return{
+              ...item,
+              quantity:item.quantity+1
+            }
+          }
+          return item
+        })
+        setCart(updatedCart)
+      }
+    
+      function decreaseQuantity(id){
+        const updatedCart=cart.map(item=>{
+          if(item.id===id && item.quantity>1){
+            return{
+              ...item,
+              quantity:item.quantity-1
+            }
+          }
+          return item
+        })
+        setCart(updatedCart)
+      }
+    
+      function clearCart(e){
+        setCart([])
+      }
+    
+    
+
+    return{
+        data,
+        cart,
+        addToCart,
+        removeFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+        clearCart
+    }
+}
+
